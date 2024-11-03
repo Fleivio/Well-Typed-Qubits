2d636043ee4ad1512b70231d1443519a967b5c24
Anomalocaris <flavioborinjunior@gmail.com>
2024-11-03 09:39:32 -0300

    more operations

--- src/Quant/QAct/QBitAct.hs
+++ src/Quant/QAct/QBitAct.hs
@@ -3,10 +3,16 @@ module QAct.QBitAct(
   , module QAct.QAct
   , h
   , x
+  , y
   , z
   , cnot
   , entangle
   , cz
+  , fredkin
+  , swap
+  , p
+  , t
+  , s
   , toffoli) where
 
 import QAct.QAct
@@ -27,11 +33,27 @@ x = qActMatrix [
       ((I:>NNil, O:>NNil), 1 :+ 0)
     ]
 
-z :: QBitAct 1 ()
-z = qActMatrix [
-      ((O:>NNil, O:>NNil), 1 :+ 0),
-      ((I:>NNil, I:>NNil), (-1) :+ 0)
+y :: QBitAct 1 ()
+y = qActMatrix [
+      ((O:>NNil, I:>NNil), (-1):+ 0),
+      ((I:>NNil, O:>NNil), 1:+ 0)
     ]
+
+
+p :: Double -> QBitAct 1 ()
+p l = qActMatrix [
+      ((O:>NNil, O:>NNil), 1:+ 0),
+      ((I:>NNil, I:>NNil), exp (0 :+ (pi*l)))
+    ]
+
+z :: QBitAct 1 ()
+z = p pi
+
+s :: QBitAct 1 ()
+s = p (pi/2)
+
+t :: QBitAct 1 ()
+t = p (pi/4)
 
 cnot :: QBitAct 2 ()
 cnot = qActMatrix [
@@ -64,4 +86,24 @@ cz = qActMatrix [
       ((O:>I:>NNil, O:>I:>NNil), 1:+ 0),
       ((I:>O:>NNil, I:>O:>NNil), 1:+ 0),
       ((I:>I:>NNil, I:>I:>NNil), (-1):+ 0)
+    ]
+
+fredkin :: QBitAct 3 ()
+fredkin = qActMatrix [
+      ((O:>O:>O:>NNil, O:>O:>O:>NNil), 1:+ 0),
+      ((O:>O:>I:>NNil, O:>O:>I:>NNil), 1:+ 0),
+      ((O:>I:>O:>NNil, O:>I:>O:>NNil), 1:+ 0),
+      ((O:>I:>I:>NNil, O:>I:>I:>NNil), 1:+ 0),
+      ((I:>O:>O:>NNil, I:>O:>O:>NNil), 1:+ 0),
+      ((I:>O:>I:>NNil, I:>I:>O:>NNil), 1:+ 0),
+      ((I:>I:>O:>NNil, I:>O:>I:>NNil), 1:+ 0),
+      ((I:>I:>I:>NNil, I:>I:>I:>NNil), 1:+ 0)
+    ]
+
+swap :: QBitAct 2 ()
+swap = qActMatrix [
+      ((O:>O:>NNil, O:>O:>NNil), 1:+ 0),
+      ((O:>I:>NNil, I:>O:>NNil), 1:+ 0),
+      ((I:>O:>NNil, O:>I:>NNil), 1:+ 0),
+      ((I:>I:>NNil, I:>I:>NNil), 1:+ 0)
     ]
